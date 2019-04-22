1ecf496193fd7d910426fcb24fb12391dcced07a
kks35 <zkak21@gmail.com>
2019-04-22 13:00:59 -0700

    Add files via upload

---
+++ Inline.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import os
+import time
+
+cap_left = cv2.VideoCapture(0)
+cap_right = cv2.VideoCapture(1)
+
+flag = 1
+num = 1
+
+while(cap_left.isOpened() ):
+    success_left, img_left = cap_left.read()
+    success_right, img_right = cap_right.read()
+
+    cv2.namedWindow("Cap_left", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("Cap_right", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Cap_left", 600, 600)
+    cv2.resizeWindow("Cap_right", 600, 600)
+    cv2.moveWindow("Cap_left", 0, 0)
+    cv2.moveWindow("Cap_right",400, 0)
+
+    cv2.imshow("Cap_left", img_left)
+    cv2.imshow("Cap_right", img_right)
+    
+    k = cv2.waitKey(1)
+    if k == ord("s"):
+        cv2.imwrite("C://Users//MEC 101//Desktop//CAM//Inline//" + str(num) + ".jpg", img_left)
+        cv2.imwrite("C://Users//MEC 101//Desktop//CAM//Inline//" + str(num) + "(2).jpg", img_right)
+        print("save successful:" + str(num) + ".jpg     " + str(num) + "(2).jpg")
+        print("_____________________")
+        num += 1
+        cv2.destroyAllWindows()
+    elif k == 27:
+        cv2.destroyAllWindows()
+        break
+cap_left.release()
+cap_right.release()
+
+    
+    
+
+
+
+
+
+
+
